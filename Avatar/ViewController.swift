d1598cabf03a2045dfcfef1f5a995a065433db84
Jacey Schow <jaceyy.schow@gmail.com>
2020-09-16 20:20:34 -0600

    Initial Commit

---
+++ Avatar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avatar
+//
+//  Created by Owner on 9/16/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
